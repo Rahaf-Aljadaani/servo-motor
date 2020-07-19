8a6c2cf6d0bbf98beaa376147460a06442057a8e
Rahaf-Aljadaani <66742756+Rahaf-Aljadaani@users.noreply.github.com>
2020-07-19 05:47:26 +0300

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+int positiion = 0;
+
+int i = 0;
+
+Servo servo;
+Servo servo_1;
+
+
+void setup()
+{
+  servo.attach(3);
+  servo_1.attach(5);
+
+}
+
+void loop()
+{
+  positiion = 0;
+  for (positiion = 1; positiion <= 179; positiion += 1) {
+    servo.write(positiion);
+    servo_1.write(179-positiion);
+    delay(15); // Wait for 20000 millisecond(s)
+  }
+  for (positiion = 179; positiion >= 1; positiion -= 1) {
+   servo.write(positiion);
+    servo_1.write(179-positiion);
+    delay(15); // Wait for 20000 millisecond(s)
+  }
+}
